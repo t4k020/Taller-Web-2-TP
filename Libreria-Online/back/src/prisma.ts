1ff10c49cb9169171aa210f902ccf5a8e4c67a43
t4k020 <t4k020@gmail.com>
2026-08-20 19:11:55 -0300

    fix prismaTS

--- Libreria-Online/back/src/prisma.ts
+++ Libreria-Online/back/src/prisma.ts
@@ -1,11 +1,20 @@
-import { PrismaPg } from "@prisma/adapter-pg";
+import pg from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import PrismaPackage from '@prisma/client';
 import { config } from './config/config.js';
-import { PrismaClient } from '@prisma/client';
 
-const adapter = new PrismaPg({
-    connectionString: config.db,
+// Extraer PrismaClient para compatibilidad ESM en Vercel
+const { PrismaClient } = PrismaPackage;
+
+// 1. Crear el Pool de conexiones con la librería 'pg'
+const pool = new pg.Pool({
+  connectionString: config.db,
 });
 
+// 2. Inicializar el adaptador de Prisma pasándole el pool
+const adapter = new PrismaPg(pool);
+
+// 3. Crear la instancia de PrismaClient
 export const prisma = new PrismaClient({
-    adapter
+  adapter,
 });
