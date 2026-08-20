5d79e4cffb148c6da9517550762f7d16805705b1
t4k020 <t4k020@gmail.com>
2026-08-20 19:49:10 -0300

    Fix DB V1

--- Libreria-Online/back/src/config/config.ts
+++ Libreria-Online/back/src/config/config.ts
@@ -3,5 +3,9 @@ import env from "env-var";
 export const config = {
 
     PORT: env.get('PORT').required().asPortNumber(),
-    db: env.get('DATABASE_URL').required().asString(),
+    db: env.get('DATABASE_URL')
+         .default(process.env.POSTGRES_PRISMA_URL || process.env.POSTGRES_URL || '')
+         .required()
+         .asString(),
+    
 }   
