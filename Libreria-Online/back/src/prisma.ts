6d93240e39e59876a3df84f4ef2320e302c9ab2d
t4k020 <t4k020@gmail.com>
2026-08-20 18:48:25 -0300

    fix P 1

--- Libreria-Online/back/src/prisma.ts
+++ Libreria-Online/back/src/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { config } from './config/config.js';
-import { PrismaClient } from './prisma/client.js';
+import { PrismaClient } from '@prisma/client';
 
 const adapter = new PrismaPg({
     connectionString: config.db,
